426daf1576560a056620c0a5b8109942c8bc4b87
Michael Zwyssig <mzwyssig@3ne.ch>
2016-12-06 13:58:03 +0100

    new reorganization of content and new content, wireframe images, etc.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,21 +1,4 @@
-// Smooth Scroll
-$(function() {
-  $('a[href*="#"]:not([href="#"])')
-    .click(function() {
-      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-        var target = $(this.hash);
-        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-        if (target.length) {
-          $('html, body')
-            .animate({
-              scrollTop: target.offset()
-                .top
-            }, 200);
-          return false;
-        }
-      }
-    });
-});
+
 
 
 // Youtube
@@ -57,3 +40,64 @@ function onYouTubeIframeAPIReady() {
     }
   });
 }
+
+
+// Tabs
+$('ul.tabs').each(function(){
+  // For each set of tabs, we want to keep track of
+  // which tab is active and its associated content
+  var $active, $content, $links = $(this).find('a');
+
+  // If the location.hash matches one of the links, use that as the active tab.
+  // If no match is found, use the first link as the initial active tab.
+  $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+  $active.addClass('active');
+
+  $content = $($active[0].hash);
+
+  // Hide the remaining content
+  $links.not($active).each(function () {
+    console.log('hiding '+this.hash);
+    $(this.hash).hide();
+  });
+
+  // Bind the click event handler
+  $(this).on('click', 'a', function(e){
+    // Prevent the anchor's default click action
+    e.preventDefault();
+    // Make the old tab inactive.
+    $active.removeClass('active');
+    $content.hide();
+
+    // Update the variables with the new link and content
+    $active = $(this);
+    $content = $(this.hash);
+
+    // Make the tab active.
+    $active.addClass('active');
+    $content.show();
+
+
+  });
+});
+
+
+
+// Smooth Scroll
+$(function() {
+  $('a[href*="#"]:not([href="#"]):not(.tablink)')
+    .click(function() {
+      if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+        var target = $(this.hash);
+        target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+        if (target.length) {
+          $('html, body')
+            .animate({
+              scrollTop: target.offset()
+                .top
+            }, 200);
+          return false;
+        }
+      }
+    });
+});
