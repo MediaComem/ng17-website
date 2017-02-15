a377fe823a62c0ebcaf6c84bafb07a74addd4a85
Raphael Baumann <wayoffproduction@gmail.com>
2017-02-15 11:56:16 +0100

    moved include outside

--- app/process.php
+++ app/process.php
@@ -1,6 +1,6 @@
 <?php
-require_once('includes/config.php');
-require_once('includes/PHPMailer/class.phpmailer.php');
+require_once('../includes/config.php');
+require_once('../includes/PHPMailer/class.phpmailer.php');
 
 function sanitize($text) {
     $text = trim($text);
