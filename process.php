595c8eded736af2fa3e46f26f9c201904ea1fdaa
Raphael Baumann <wayoffproduction@gmail.com>
2017-02-15 17:04:53 +0100

    updateeee

--- process.php
+++ process.php
@@ -1,6 +1,7 @@
 <?php
 require_once('includes/config.php');
 require_once('includes/PHPMailer/class.phpmailer.php');
+require_once('includes/PHPMailer/class.smtp.php');
 
 function sanitize($text) {
     $text = trim($text);
