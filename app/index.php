72b73f78107fefcc6343ff60906dfab261c754a9
Raphael Baumann <wayoffproduction@gmail.com>
2017-03-08 08:32:25 +0100

    updated to google tag manager

--- app/index.php
+++ app/index.php
@@ -35,6 +35,14 @@ if (isset($_SESSION['return_data'])) {
     <meta charset="utf-8">
     <meta name="description" content="Tente l'expérience du vidéo mapping, en grandeur nature !">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-TTFCNHC');</script>
+	<!-- End Google Tag Manager -->
 
     <title>Numerik Games 2017</title><!-- Place favicon.ico in the root directory -->
     <!-- build:css styles/vendor.css -->
@@ -55,6 +63,12 @@ if (isset($_SESSION['return_data'])) {
     <!--[if lt IE 10]>
       <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
+    <!-- Google Tag Manager (noscript) -->
+	<noscript>
+		<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TTFCNHC"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe>
+	</noscript>
+	<!-- End Google Tag Manager (noscript) -->
 
     <section id="landing" class="fullscreen home negative">
         <div class="triangle-topleft"></div>
@@ -235,17 +249,7 @@ if (isset($_SESSION['return_data'])) {
                 </div>
             </div>
         </div>
-    </section><!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-    <script type="text/javascript">
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-	    ga('create', 'UA-91100429-1', 'auto');
-	    ga('send', 'pageview');
-
-    </script>
+    </section>
     <!-- build:js scripts/vendor.js -->
     <!-- bower:js -->
     <script src="/bower_components/jquery/dist/jquery.js"></script>
--- app/kit.php
+++ app/kit.php
@@ -35,6 +35,14 @@ if (isset($_SESSION['return_data'])) {
     <meta charset="utf-8">
     <meta name="description" content="Tente l'expérience du vidéo mapping, en grandeur nature !">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <!-- Google Tag Manager -->
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+	new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+	'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,'script','dataLayer','GTM-TTFCNHC');</script>
+	<!-- End Google Tag Manager -->
 
     <title>Numerik Games 2017 - Eclairâges kit</title><!-- Place favicon.ico in the root directory -->
     <!-- build:css styles/vendor.css -->
@@ -52,6 +60,14 @@ if (isset($_SESSION['return_data'])) {
 </head>
 
 <body>
+	
+	<!-- Google Tag Manager (noscript) -->
+	<noscript>
+		<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TTFCNHC"
+		height="0" width="0" style="display:none;visibility:hidden"></iframe>
+	</noscript>
+	<!-- End Google Tag Manager (noscript) -->
+	
     <!--[if lt IE 10]>
       <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
     <![endif]-->
@@ -258,16 +274,6 @@ if (isset($_SESSION['return_data'])) {
 		</div>
 	</section>
 
-    <script type="text/javascript">
-		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-	    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-	    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-	    })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-	    ga('create', 'UA-91100429-1', 'auto');
-	    ga('send', 'pageview');
-
-    </script>
     <!-- build:js scripts/vendor.js -->
     <!-- bower:js -->
     <script src="/bower_components/jquery/dist/jquery.js"></script>
